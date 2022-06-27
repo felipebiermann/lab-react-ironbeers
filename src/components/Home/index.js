612ede5fe754bb2fd83646b6c1e9fc09a946b50b
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-06-27 20:20:20 -0300

    mais estilizado

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,51 +5,46 @@ import ImgNewBeer from "../../assets/new-beer.png";
 
 export function Home() {
   return (
-    <div>
+    <div className="card" style={{ width: "18rem" }}>
       <Link to={"/beers"}>
-        <img src={imgBeers} alt="Img Beers" />
+        <img src={imgBeers} className="card-img-top" alt="Img Beers" />
       </Link>
-      <div>
-        <h1>Beers</h1>
-        <h5>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
-        </h5>
+      <div className="class-body">
+        <h1 className="card-title">All Beers</h1>
+        <h3 className="card-text">
+          Aqui você encontra todas as cervejas JUNTAS!
+        </h3>
+
+        <a href="beers" className="btn btn-primary">
+          <button style={{ cursor: "pointer" }}>Saiba Mais!</button>
+        </a>
+
+        <hr />
       </div>
-      <Link to={"/new-beer"}>
-        <img src={ImgNewBeer} alt="Img New Beers" />
-      </Link>
-      <div>
-        <h1>New Beers</h1>
-        <h5>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
-        </h5>
+      <div className="card" style={{ width: "18rem" }}>
+        <Link to={"/new-beer"}>
+          <img src={ImgNewBeer} className="card-img-top" alt="Img New Beers" />
+        </Link>
+        <div>
+          <h1>New Beers</h1>
+          <h3>Adicione novas Cervejas!</h3>
+          <a href="new-beer" className="btn btn-primary">
+            <button style={{ cursor: "pointer" }}>Saiba Mais!</button>
+          </a>
+        </div>
       </div>
-      <Link to={"/random-beer"}>
-        <img src={imgRandomBeer} alt="Random Beer" />
-      </Link>
-      <div>
-        <h1>Random Beers</h1>
-        <h5>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
-        </h5>
+      <hr />
+      <div className="card" style={{ width: "18rem" }}>
+        <Link to={"/random-beer"}>
+          <img src={imgRandomBeer} className="card-img-top" alt="Random Beer" />
+        </Link>
+        <div>
+          <h1>Random Beers</h1>
+          <h3>Aqui é o lugar para ter cervejas aleatórias!</h3>
+          <a href="random-beer" className="btn btn-primary">
+            <button style={{ cursor: "pointer" }}>Saiba Mais!</button>
+          </a>
+        </div>
       </div>
     </div>
   );
